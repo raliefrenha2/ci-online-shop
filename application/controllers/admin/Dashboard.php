a5f2924746a0533125a73350248f75dbc4a30ba6
ai-romi <raliefrenha@gmail.com>
2019-04-02 01:19:21 +0700

    add adminlte template

---
+++ application/controllers/admin/Dashboard.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+	public function index()
+	{
+		$data = [ 'title' => 'Halaman Administrator',
+					'isi' => 'admin/dashboard/list'
+		];
+
+		$this->load->view('admin/layouts/wrapper', $data, FALSE);
+	}
+
+}
+
+/* End of file Dashboard.php */
+/* Location: ./application/controllers/admin/Dashboard.php */
